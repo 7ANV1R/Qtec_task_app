f68e3de07fd8d9fc351a23e06f7f747851411754
Tanvir <ibnmizan7878@gmail.com>
2022-09-21 20:34:01 +0600

    added demo product card design

--- lib/presentation/pages/search/search_view.dart
+++ lib/presentation/pages/search/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qtec_task_app/core/const/assets_value.dart';
+import 'package:qtec_task_app/presentation/pages/search/widget/product_card.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
 class SearchView extends StatelessWidget {
@@ -8,25 +9,83 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'কাঙ্ক্ষিত পণ্যটি খুঁজুন',
-                suffixIcon: WebsafeSvg.asset(
-                  kAssetSearchIcon,
-                  height: 16,
-                  width: 16,
-                  fit: BoxFit.scaleDown,
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(64),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'কাঙ্ক্ষিত পণ্যটি খুঁজুন',
+                    suffixIcon: WebsafeSvg.asset(
+                      kAssetSearchIcon,
+                      height: 16,
+                      width: 16,
+                      fit: BoxFit.scaleDown,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ],
+          ),
+        ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+            itemCount: 52,
+            physics: const BouncingScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisExtent: 300,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+            ),
+            itemBuilder: (context, index) => const ProductCard(),
+          ),
         ),
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(),
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(16.0),
+    //     child: SingleChildScrollView(
+    //       child: Column(
+    //         children: [
+    //           TextFormField(
+    //             decoration: InputDecoration(
+    //               hintText: 'কাঙ্ক্ষিত পণ্যটি খুঁজুন',
+    //               suffixIcon: WebsafeSvg.asset(
+    //                 kAssetSearchIcon,
+    //                 height: 16,
+    //                 width: 16,
+    //                 fit: BoxFit.scaleDown,
+    //               ),
+    //             ),
+    //           ),
+    //           kVerticalSpaceL,
+    //           ListView(
+    //             children: [],
+    //           ),
+    //           GridView.builder(
+    //             shrinkWrap: true,
+    //             itemCount: 20,
+    //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    //               crossAxisCount: 2,
+    //               crossAxisSpacing: 16,
+    //               mainAxisSpacing: 16,
+    //             ),
+    //             itemBuilder: (context, index) => Text('data'),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
---
+++ lib/presentation/pages/search/widget/product_card.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:qtec_task_app/core/const/ui_helper.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    TextTheme textTheme = themeData.textTheme;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.network(
+              'https://d1ojphegpburrh.cloudfront.net/media/images/product/None/17/Rupchanda_Soybean_Oil__5_ltr.png',
+              height: 117,
+              fit: BoxFit.fill,
+            ),
+            Text(
+              'লেস ক্লাসিক ফ্যামিলি সাইজ চিপস্',
+              textAlign: TextAlign.center,
+              style: textTheme.bodyText2,
+            ),
+            Row(
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'ক্রয়',
+                        style: textTheme.bodyText2!.copyWith(fontSize: 8, fontWeight: FontWeight.w400),
+                      ),
+                      const WidgetSpan(child: kHorizontalSpaceXS),
+                      TextSpan(
+                        text: '৳ 20.00',
+                        style: textTheme.headline2!.copyWith(
+                          color: themeData.primaryColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Expanded(child: kHorizontalSpaceS),
+                Text(
+                  '৳ 22.00',
+                  style: textTheme.bodyText2!.copyWith(
+                    fontSize: 12,
+                    color: themeData.primaryColor,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'বিক্রয়',
+                        style: textTheme.bodyText2!.copyWith(fontSize: 8, fontWeight: FontWeight.w400),
+                      ),
+                      const WidgetSpan(child: kHorizontalSpaceXS),
+                      TextSpan(
+                        text: '৳ 25.00',
+                        style: textTheme.headline2!.copyWith(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Expanded(child: kHorizontalSpaceS),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'লাভ',
+                        style: textTheme.bodyText2!.copyWith(fontSize: 8, fontWeight: FontWeight.w400),
+                      ),
+                      const WidgetSpan(child: kHorizontalSpaceXS),
+                      TextSpan(
+                        text: '৳ 5.00',
+                        style: textTheme.headline2!.copyWith(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
